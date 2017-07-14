74f7bded3bcd2d9693f4358607c7cb7d24a2f1b5
huhuang03 <huhuang03@gmail.com>
2017-07-14 11:30:03 +0800

    auto commit

--- sync_websocket/src/main/java/com/th/snws/WebSocketClientWrap.java
+++ sync_websocket/src/main/java/com/th/snws/WebSocketClientWrap.java
@@ -12,7 +12,9 @@ import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,17 +23,31 @@ import java.util.Map;
 
 public class WebSocketClientWrap {
     private WebSocketClient     mClient;
-    private boolean             isOpen   = false;
-    private Map<Integer, Order> orderMap = new HashMap<>();
-    private Handler mHander;
+    private boolean             hasConnected = false;
+    private Map<Integer, Order> orderMap     = new HashMap<>();
+    private Handler mHandler;
+
+    private List<Runnable> cachedTasks = new ArrayList<>();
+
+    private boolean isConnecting = false;
+
+    private final Object lock = new Object();
 
     public WebSocketClientWrap(String url) {
-        mHander = new Handler(Looper.getMainLooper());
+        mHandler = new Handler(Looper.getMainLooper());
 
         mClient = new WebSocketClient(URI.create(url)) {
+
             @Override
             public void onOpen(ServerHandshake handshakedata) {
-                isOpen = true;
+                hasConnected = true;
+                isConnecting = false;
+                synchronized (lock) {
+                    for (Runnable cachedTask : cachedTasks) {
+                        cachedTask.run();
+                    }
+                    cachedTasks.clear();
+                }
             }
 
             @Override
@@ -41,7 +57,7 @@ public class WebSocketClientWrap {
                 final Order order = orderMap.get(rstMsg.id);
 
                 if (order.callback != null) {
-                    mHander.post(new Runnable() {
+                    mHandler.post(new Runnable() {
                         @Override
                         public void run() {
                             order.callback.onSuccess(new Gson().fromJson(rstMsg.msg, order.responseClass));
@@ -52,24 +68,26 @@ public class WebSocketClientWrap {
 
             @Override
             public void onClose(int code, String reason, boolean remote) {
-                isOpen = false;
+                hasConnected = false;
+                isConnecting = false;
                 failedAll();
             }
 
             @Override
             public void onError(Exception ex) {
-                isOpen = false;
+                hasConnected = false;
+                isConnecting = false;
                 failedAll();
             }
 
             private synchronized void failedAll() {
-                mHander.post(new Runnable() {
+                mHandler.post(new Runnable() {
                     @Override
                     public void run() {
                         for (Integer key : orderMap.keySet()) {
                             Order order = orderMap.get(key);
                             if (order != null && !order.hasFinish) {
-                                order.callback.onFailed();
+                                order.callback.onFailed(WebSocketServerCallback.ErrCode.ERR_CANT_CONNECT, WebSocketServerCallback.ErrCode.MSG_CANT_CONNECT);
                             }
                         }
                         orderMap.clear();
@@ -77,12 +95,29 @@ public class WebSocketClientWrap {
                 });
             }
         };
-        mClient.connect();
+        connect();
     }
 
+    private void connect() {
+        synchronized (lock) {
+            if (!isConnecting) {
+               mClient.connect();
+                isConnecting = true;
+            }
+        }
+    }
+
+    /**
+     * 服务器回应
+     */
     public interface WebSocketServerCallback<T> {
         void onSuccess(T rst);
-        void onFailed();
+        void onFailed(int errCode, String errMsg);
+
+        interface ErrCode {
+            int ERR_CANT_CONNECT = 1;
+            String MSG_CANT_CONNECT = "socket连接失败";
+        }
     }
 
     /*-------------------------------------------*/
@@ -96,18 +131,34 @@ public class WebSocketClientWrap {
      * @param clazz 服务器返回的bean
      * @param <T> 需要将服务端返回数据返json成什么类型
      */
-    public <T>void send(Object msg, Class<T> clazz, WebSocketServerCallback<T> callback) {
-        if (msg == null || !isOpen) {
-            if (callback != null) {
-                callback.onFailed();
+    public <T>void send(final Object msg, final Class<T> clazz, final WebSocketServerCallback<T> callback) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                String toSend = new Gson().toJson(msg);
+                MsgWrap msgWrap = obtainMsg(toSend);
+                mClient.send(new Gson().toJson(msgWrap));
+                orderMap.put(msgWrap.id, new Order(msgWrap, clazz, callback));
             }
-            return;
+        };
+        if (hasConnected) {
+            task.run();
+        } else  {
+            synchronized (lock) {
+                cachedTasks.add(task);
+            }
+            if (!isConnecting) {
+                connect();
+            }
         }
 
-        String toSend = new Gson().toJson(msg);
-        MsgWrap msgWrap = obtainMsg(toSend);
-        mClient.send(new Gson().toJson(msgWrap));
-        orderMap.put(msgWrap.id, new Order(msgWrap, clazz, callback));
+//        if (msg == null || !hasConnected) {
+//            if (callback != null) {
+//                callback.onFailed(WebSocketServerCallback.ErrCode.ERR_CANT_CONNECT, WebSocketServerCallback.ErrCode.MSG_CANT_CONNECT);
+//            }
+//            return;
+//        }
+
     }
 
     public void send(Object msg, WebSocketServerCallback<String> callback) {
